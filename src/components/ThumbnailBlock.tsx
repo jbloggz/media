97ede24c1c4d900aae416f8701846a1cb469fa7a
Josef Barnes <joe@barnesmail.net>
2024-02-08 23:14:47 +1000

    Adds thumbnail image component and route to download thumbnails

--- src/components/ThumbnailBlock.tsx
+++ src/components/ThumbnailBlock.tsx
@@ -7,10 +7,12 @@
  */
 
 import { forwardRef } from 'react';
+import { ThumbnailImage } from '.';
 
 interface ThumbnailBlockProps {
    className?: string;
-   heading?: string;
+   heading: string;
+   showHeading: boolean;
    start: number;
    end: number;
 }
@@ -21,18 +23,8 @@ const ThumbnailBlock = forwardRef<HTMLDivElement, ThumbnailBlockProps>(function 
          {props.heading && <h1 className="text-xl font-bold p-2">{props.heading}</h1>}
          <div className="pb-1 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-1">
             {Array.from(Array(props.end - props.start)).map((_, i) => (
-               <div key={`${props.heading}-${i + props.start}`} className="w-full aspect-square">
-                  <div className="flex items-center justify-center w-full h-full bg-gray-300 rounded dark:bg-gray-700">
-                     <svg
-                        className="w-10 h-10 text-gray-200 dark:text-gray-600"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 20 18"
-                     >
-                        <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-                     </svg>
-                  </div>
+               <div key={`${props.heading}-${i + props.start}`} className="relative w-full aspect-square rounded overflow-hidden">
+                  <ThumbnailImage block={props.heading} index={i} />
                </div>
             ))}
          </div>
