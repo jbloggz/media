fb9d29047848f71a02479e0bda2c8250288da411
Josef Barnes <joe@barnesmail.net>
2024-02-10 13:59:58 +1000

    Adds database schema

---
+++ schema.sql
@@ -0,0 +1,48 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2024 Josef Barnes
+ *
+ * schema.sql: This file is the database schema for the media database
+ */
+
+/* A table to store the media items */
+CREATE TABLE IF NOT EXISTS media (
+   id          SERIAL8 PRIMARY KEY,
+   path        TEXT NOT NULL UNIQUE, -- path relative to root level
+   type        TEXT NOT NULL,        -- mime type (either image or video)
+   timestamp   INT8 NOT NULL,        -- in epoch seconds
+   size        INT4 NOT NULL,        -- file size (in bytes)
+   width       INT4 NOT NULL,        -- in pixels
+   height      INT4 NOT NULL,        -- in pixels
+   duration    INT4 DEFAULT NULL,    -- video duration in milliseconds (NULL for images)
+   latitude    REAL DEFAULT NULL,    -- in degrees
+   longitude   REAL DEFAULT NULL,    -- in degrees
+   make        TEXT DEFAULT NULL,    -- make of the camera (NULL if unknown or N/A)
+   model       TEXT DEFAULT NULL,    -- model of the camera (NULL if unknown or N/A)
+   sha256      TEXT NOT NULL         -- SHA256 checksum for the file
+);
+CREATE INDEX IF NOT exists media_path_idx ON media(path);
+CREATE INDEX IF NOT exists media_type_idx ON media(type);
+CREATE INDEX IF NOT exists media_timestamp_idx ON media(timestamp);
+CREATE INDEX IF NOT exists media_camera_idx ON media(make, model);
+
+/* A table for caching the blocks of media files */
+CREATE TABLE IF NOT EXISTS blocks (
+   id          SERIAL8 PRIMARY KEY,
+   name        TEXT NOT NULL,        -- name of the block
+   count       INT4 NOT NULL,        -- number of files in the block
+   total       INT4 NOT NULL         -- total files in this an all previous blocks
+);
+
+/* A table for storing running processes and progress */
+CREATE TABLE IF NOT EXISTS progress (
+   name        TEXT PRIMARY KEY,     -- name of the process
+   message     JSON DEFAULT NULL     -- message for current process
+);
+
+/* A table to store the push subscriptions */
+CREATE TABLE IF NOT EXISTS push_subscription (
+   id              SERIAL8 PRIMARY KEY,
+   value           TEXT NOT NULL UNIQUE   -- The subscription data
+);
